35728640e5f8cb05d08ecc660222706903bcf278
Andrew <ghobrial.andrew@gmail.com>
2022-02-22 08:17:18 -0500

    grrr

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const TorrentSearchApi = require('torrent-search-api');
 
 TorrentSearchApi.enableProvider('ThePirateBay');
@@ -12,14 +12,13 @@ var torrentSearch = async function(query) {
     return torrents;
   }
 
+app.get('/', (req, res) => res.send('Home Page Route'));
+
 app.get('/api/torrentsearch/', (req, res) => {
     console.log('the req.query.query is: ', req.query.query)
     const response = torrentSearch(req.query.query);
     res.send(response)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
 module.exports = app
